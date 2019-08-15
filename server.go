0f177f5faeda0798ef22aaead51a483af382c1f4
J.P. Zivalich <j.p.zivalich@gmail.com>
2019-08-15 10:25:36 -0400

    Move server to separate file and remove config pkg (#3)
    * Move server to separate file
    
    * Remove config package

---
+++ server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/jpz13/service/api"
+	"github.com/jpz13/service/core"
+)
+
+type server struct {
+	apiHandler http.Handler
+	listenPort int
+	serv       *http.Server
+	config     *serverConfig
+	logger     log.Logger
+}
+
+// serverConfig holds all config needed to start a support analytics service instance
+type serverConfig struct {
+	ListenPort int
+	Logger     log.Logger
+}
+
+func newServer(config *serverConfig) (*server, error) {
+	dummyService := core.NewDummyService(&core.Config{})
+
+	apiHandler := api.New(&api.Config{
+		DummyService: dummyService,
+		Logger:       config.Logger,
+	})
+
+	return &server{
+		apiHandler: apiHandler,
+		listenPort: config.ListenPort,
+		logger:     config.Logger,
+		config:     config,
+	}, nil
+}
+
+func (s *server) start() {
+	addr := net.JoinHostPort("0.0.0.0", strconv.Itoa(s.listenPort))
+
+	serv := &http.Server{
+		Addr:         addr,
+		Handler:      s.apiHandler,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	log.Println("Starting API server")
+
+	go func() {
+		err := serv.ListenAndServe()
+		// expected error on shutdown
+		if err == http.ErrServerClosed {
+			log.Printf("ListenAndServe response: %v", err)
+		} else {
+			log.Fatal("API server listen failed")
+		}
+	}()
+
+	s.serv = serv
+
+	s.logger.Print("Started API server")
+}
+
+func (s *server) stop() error {
+	s.logger.Print("Stopping server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := s.serv.Shutdown(ctx)
+
+	s.logger.Print("Server stopped")
+	return err
+}
